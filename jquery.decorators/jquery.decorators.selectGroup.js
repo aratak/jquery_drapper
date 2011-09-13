c41d738b09f4865d368a955f3675b9764f86a9a1
Alexey Osipenko <aratak@anahoret.com>
2011-09-13 14:47:21 +0300

    add decorator selectGroup. It can replace select with radio/checkbox group.

---
+++ jquery.decorators/jquery.decorators.selectGroup.js
@@ -0,0 +1,47 @@
+(function($) {
+  if($.fn.decorator === undefined) {
+    throw "$.decorator plugin hasn't been required"
+        + "This plugin depends from 'jquery.decorator' plugin. "
+        + "See https://github.com/aratak/jquery.decorator/ for details."
+  }
+
+  $.decorators = $.extend($.decorators, {
+    selectGroup: function() {
+      var decoree = this;
+      var decoreeType = $(decoree).attr('multiple') ? "checkbox" : "radio";
+      var createListItem = function(optionTag, i) {
+        var liTag = $(document.createElement("li"));
+        optionTag = $(optionTag);
+
+        var inputTag = $(document.createElement("input")).attr({
+          type: decoreeType,
+          name: $(decoree).attr('name'),
+          id: $(decoree).attr('id') + "_" + i,
+          value: optionTag.val(),
+          selected: optionTag.is(':selected')
+        });
+
+        var labelTag = $(document.createElement("label")).attr({
+          for: $(decoree).attr('id') + "_" + i
+        }).html(optionTag.val());
+
+        return liTag.append(inputTag).append(labelTag);
+      };
+
+      var removeDecoree = function() {
+        return $(decoree).remove();
+      }
+
+      var list = $(document.createElement("ul")).attr({
+        'data-decorator-list': true
+      }).addClass('decoratorList decoratorRadioGroup');
+
+
+      $.each($(decoree).find('option'), function(i, optionTag) {
+        list.append(createListItem(optionTag, i));
+      });
+
+      return decoree.wrapper().append(list) && removeDecoree();
+    }
+  });
+})(jQuery)
