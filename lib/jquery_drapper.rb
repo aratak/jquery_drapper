94cfba50257a59abc1546ff4897b6c7b96d27db1
Alexey Osipenko <aratak@anahoret.com>
2011-12-22 10:52:25 +0200

    Add Rails::Engine
    Rails::Engine move to sparate file.
    
    Add Rails::Engine
    
    Rails::Engine move to sparate file.
    
    require 'rails/engine'
    
    Rails::Engine -> module Rails; class Engine

--- lib/jquery_drapper.rb
+++ lib/jquery_drapper.rb
@@ -1,4 +1,7 @@
 require "jquery_drapper/version"
 
 module JqueryDrapper
+  if defined?(::Rails) and ::Rails.version >= "3.1"
+    require 'jquery_drapper/rails/engine'
+  end
 end
---
+++ lib/jquery_drapper/rails/engine.rb
@@ -0,0 +1,6 @@
+# this class enables the asset pipeline
+module JqueryDrapper
+  module Rails
+    class Engine < ::Rails::Engine ; end
+  end
+end
