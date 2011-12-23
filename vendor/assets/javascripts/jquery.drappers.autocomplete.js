88b017aa864d28660ecc077b17f2f24ae7ebcb1a
Alexey Osipenko <aratak@anahoret.com>
2011-12-23 17:24:36 +0200

    Introduce drapper autocompelte

--- vendor/assets/javascripts/jquery.drappers.autocomplete.js
+++ vendor/assets/javascripts/jquery.drappers.autocomplete.js
@@ -13,6 +13,48 @@
 
       var decoree = this;
 
+      var getData = function() {
+        return $(decoree).find('option').map(function() {
+          return {
+            queue: $(this).attr('data-drapper-keywords'),
+            hint: $(this).attr('data-drapper-hint'),
+            value: $(this).html()
+          }
+        });
+      }
+      var optionsToTokenInput = function() {
+        return {
+          theme: 'facebook',
+          propertyToSearch: "queue",
+          resultsFormatter: function(item) { return "<li>" + item.hint + "</li>" },
+          tokenFormatter: function(item) { return "<li>" + item.value + "</li>" }
+        }
+      }
+
+      var tokenInput = function() {
+        var findTokenInput = function() {
+          decoree.wrapper().find('input[data-drapper-autocomplete-container]');
+        }
+        var createTokenInput = function() {
+          return $(document.createElement('input')).attr({
+            'type': 'text',
+            'data-drapper-autocomplete-container': true
+          }).appendTo(decoree.wrapper());
+        }
+        return findTokenInput() || createTokenInput();
+      }
+
+      var hideDecoree = function() {
+        $(decoree).css({
+          visibility: 'hidden',
+          position: 'absolute',
+          zIndex: -1
+        })
+        return true;
+      };
+
+      hideDecoree();
+      $(tokenInput()).tokenInput(getData(), optionsToTokenInput());
     }
   });
 })(jQuery);
