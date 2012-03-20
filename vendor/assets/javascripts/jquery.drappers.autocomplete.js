8795702b03a41ed3da59536407857f66c0fefd19
Alexey Osipenko <alexey@osipenko.in.ua>
2012-03-20 11:12:12 +0200

    Fire event change on decoree after select attribute

--- vendor/assets/javascripts/jquery.drappers.autocomplete.js
+++ vendor/assets/javascripts/jquery.drappers.autocomplete.js
@@ -1,70 +1,72 @@
-(function($) {
+(function ($) {
   $.drappers = $.extend($.drappers, {
-    autocomplete: function() {
-      if(!$.isFunction($.fn.sortable)) {
-        throw "Jquery plugin '$.fn.tokenInput' hasn't been required. "
-            + "Check http://loopj.com/jquery-tokeninput/ "
+    autocomplete: function () {
+      if (!$.isFunction($.fn.sortable)) {
+        throw "Jquery plugin '$.fn.tokenInput' hasn't been required. " +
+              "Check http://loopj.com/jquery-tokeninput/ ";
       }
 
-      if(!this.isDecoree) {
-        throw "The object is not decoree!"
-            + "See https://github.com/aratak/jquery.drapper/ for details."
+      if (!this.isDecoree) {
+        throw "The object is not decoree!" +
+              "See https://github.com/aratak/jquery.drapper/ for details.";
       }
 
       var decoree = this;
 
-      var getData = function(selectSelected) {
-        return $(decoree).find('option' + (selectSelected ? '[selected]' : '')).map(function() {
+      var getData = function (selectSelected) {
+        return $(decoree).find('option' + (selectSelected ? '[selected]' : '')).map(function () {
           return {
             id: $(this).val(),
             queue: $(this).attr('data-drapper-keywords'),
             hint: $(this).attr('data-drapper-hint'),
             value: $(this).html()
-          }
+          };
         });
-      }
+      };
 
-      var getOptionByItem = function(item) {
-        return $(decoree).find('option[value=' + item['id'] + ']')
-      }
+      var getOptionByItem = function (item) {
+        return $(decoree).find('option[value=' + item['id'] + ']');
+      };
 
-      var optionsToTokenInput = function() {
+      var optionsToTokenInput = function () {
         return $.extend({
           theme: 'facebook',
           propertyToSearch: "queue",
-          resultsFormatter: function(item) { return "<li>" + item.hint + "</li>" },
-          tokenFormatter: function(item) { return "<li>" + item.value + "</li>" },
+          resultsFormatter: function (item) { return "<li>" + item.hint + "</li>"; },
+          tokenFormatter: function (item) { return "<li>" + item.value + "</li>"; },
           preventDuplicates: true,
           hintText: "Type in a search term",
-          onAdd: function(item) {
-            return getOptionByItem(item).attr('selected', 'selected');
+          onAdd: function (item) {
+            getOptionByItem(item).attr('selected', 'selected');
+            $(decoree).trigger('change');
           },
-          onDelete: function(item) {
-            return getOptionByItem(item).removeAttr('selected');
+          onDelete: function (item) {
+            getOptionByItem(item).removeAttr('selected');
+            $(decoree).trigger('change');
           },
           prePopulate: getData(true)
         }, decoree.config);
-      }
+      };
 
-      var tokenInput = function() {
-        var findTokenInput = function() {
+      var tokenInput = function () {
+        var findTokenInput = function () {
           decoree.wrapper().find('input[data-drapper-autocomplete-container]');
-        }
-        var createTokenInput = function() {
+        };
+        var createTokenInput = function () {
           return $(document.createElement('input')).attr({
             'type': 'text',
             'data-drapper-autocomplete-container': true
           }).appendTo(decoree.wrapper());
-        }
+        };
         return findTokenInput() || createTokenInput();
-      }
+      };
 
-      var hideDecoree = function() {
+      var hideDecoree = function () {
         $(decoree).css({
           visibility: 'hidden',
           position: 'absolute',
           zIndex: -1
-        })
+        });
         return true;
       };
 
@@ -72,5 +74,5 @@
       $(tokenInput()).tokenInput(getData(), optionsToTokenInput());
     }
   });
-})(jQuery);
+}(jQuery));
 
